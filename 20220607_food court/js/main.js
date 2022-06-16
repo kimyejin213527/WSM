7aa9c2de4517948bfd03c658dca57fb02df2d1ae
gaeul <chlrkdmf0805@naver.com>
2022-06-16 14:17:22 +0900

    가을이 거 복붙

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -2,9 +2,16 @@
 const toggleMenu =  (toggleId, navlisteId)=>{
     const toggle = document.getElementById(toggleId);
     const navList = document.getElementById(navlisteId);
+    const toggleIcon = document.getElementsByTagName('i')[0];
 
     if(toggle && navList){
-        toggle.addEventListener('click', ()=> navList.classList.toggle('show-menu'))        ;
+        toggle.addEventListener('click', ()=>{
+            navList.classList.toggle('show-menu');
+            toggleIcon.classList.toggle("bx-menu");
+            toggleIcon.classList.toggle("bx-x-circle");
+
+        }
+        );
     }
 }
 toggleMenu('nav-toggle','nav-list');
